7ac1e589e1d548de13bab3ee759cad736b8e698b
Murillo Mtz <murillo.santos@ucsal.edu.br>
2020-07-24 09:36:40 -0300

    Mostrando as categorias com fotos

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,3 +1,5 @@
+import { API_CONFIG } from './../../config/api.config';
+import { CategoriaDTO } from './../../models/categoria.dto';
 import { CategoriaService } from './../../services/domain/categoria.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -16,21 +18,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+  items: CategoriaDTO[];
+
   constructor(
     public navCtrl: NavController,
-     public navParams: NavParams,
-     public categoriaService: CategoriaService) {
+    public navParams: NavParams,
+    public categoriaService: CategoriaService) {
   }
 
   ionViewDidLoad() {
     this.categoriaService.findAll().subscribe(response => {
-      console.log(response);
+      this.items = response;
     },
-    error => {
-      console.log(error);
-    }
+      error => {
+        console.log(error);
+      }
     );
   }
 
-  
+
 }
