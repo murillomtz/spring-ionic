496758eebbdec2ba2cd5826b395576eeaea350ed
Murillo Mtz <murillo.santos@ucsal.edu.br>
2020-07-27 08:22:55 -0300

    Carregando produtos de uma dada categoria

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -31,9 +31,12 @@ export class CategoriasPage {
     this.categoriaService.findAll().subscribe(response => {
       this.items = response;
     },
-      error => {}
+      error => { }
     );
   }
 
+  showProdutos(categoria_id: string) {
+    this.navCtrl.push('ProdutosPage', { categoria_id: categoria_id });
+  }
 
 }
