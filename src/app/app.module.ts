9cee87c37c33a7f13e43c7c6a6a7c17187cec049
Murillo Mtz <murillo.santos@ucsal.edu.br>
2020-07-27 17:05:30 -0300

    Criando pagina de carrinho de compras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,24 @@
-import { ProdutoService } from './../services/domain/produto.service';
-import { AuthInterceptorProvider } from './../interceptors/auth-interceptor';
-import { ClienteService } from './../services/domain/cliente.service';
-import { StorageService } from './../services/storage.service';
-import { AuthService } from './../services/auth.service';
-import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
-import { CategoriaService } from './../services/domain/categoria.service';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { HttpClientModule } from '@angular/common/http';
+
 import { MyApp } from './app.component';
-
-
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { CategoriaService } from '../services/domain/categoria.service';
+import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
+import { AuthService } from '../services/auth.service';
+import { StorageService } from '../services/storage.service';
+import { ClienteService } from '../services/domain/cliente.service';
+import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
+import { ProdutoService } from '../services/domain/produto.service';
+import { CartService } from '../services/domain/cart.service';
 
 @NgModule({
   declarations: [
-    MyApp,
-
-
+    MyApp
   ],
   imports: [
     BrowserModule,
@@ -29,7 +27,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
+    MyApp
   ],
   providers: [
     StatusBar,
@@ -41,7 +39,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AuthService,
     StorageService,
     ClienteService,
-    ProdutoService
+    ProdutoService,
+    CartService
   ]
 })
 export class AppModule { }
