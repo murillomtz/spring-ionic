287514740e6fed3938c55e9b469fd38d0b44c640
Murillo Mtz <murillo.santos@ucsal.edu.br>
2020-07-29 08:44:30 -0300

    Infinite scroll

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,30 +1,30 @@
-import { Observable } from 'rxjs/Rx';
-import { API_CONFIG } from './../../config/api.config';
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
+import { API_CONFIG } from '../../config/api.config';
+import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { ProdutoDTO } from '../../models/produto.dto';
 
 @Injectable()
 export class ProdutoService {
-    constructor(public http: HttpClient) {
 
-    }
-    findById(produto_id: string) {
-        return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
-    }
+  constructor(public http: HttpClient) {
+  }
 
-    findByCategoria(categoria_id: string) {
-        return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`)
-    }
+  findById(produto_id : string) {
+    return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
+  }
 
-    getSmallImageFromBucket(id: string): Observable<any> {
-        console.log("id item" + id);
-        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
-        return this.http.get(url, { responseType: 'blob' });
-    }
+  findByCategoria(categoria_id : string, page : number = 0, linesPerPage : number = 24) {
+    return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}&page=${page}&linesPerPage=${linesPerPage}`);
+  }
 
-    getImageFromBucket(id: string): Observable<any> {
-        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`
-        return this.http.get(url, { responseType: 'blob' });
-    }
+  getSmallImageFromBucket(id : string) : Observable<any> {
+    let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+    return this.http.get(url, {responseType : 'blob'});
+  }  
+
+  getImageFromBucket(id : string) : Observable<any> {
+    let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`
+    return this.http.get(url, {responseType : 'blob'});
+  }  
 }
